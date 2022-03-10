e01ff0e3547358612c7a922b019a27c794e52bdc
RickySmith22 <rickysmith22@gmail.com>
2022-03-10 09:17:28 -0800

    frontend for the bookstore

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Home from './pages/home'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+      <Home />
+      </div>
+    );
+  }
+}
